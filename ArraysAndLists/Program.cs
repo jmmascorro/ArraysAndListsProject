8cd77d3a27704cdad4dab01e469744ea93769746
jmmascorro <jessemascorro88@gmail.com>
2022-01-25 10:53:29 -0600

    finished exercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -27,6 +28,34 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+
+            var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var evens = new List<int>();
+            var odds = new List<int>();
+            foreach(var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"{item} is an even number");
+            }
+
+            foreach (var item in odds)
+            {
+                Console.WriteLine($"{item} is an odd number");
+            }
+                
         }
+
     }
 }
